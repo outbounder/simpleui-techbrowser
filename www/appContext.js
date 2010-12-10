976cfaaacc6e2cf59f4d46e3fb3868f39275a496
outbounder <obiwonn@gmail.com>
2010-12-11 01:13:36 +0200

    added some tiny animation effects

--- www/appContext.js
+++ www/appContext.js
@@ -2,13 +2,26 @@ var AppContext =  function() {
 	var endpoint = "http://api-techbrowser.appspot.com/", //"http://localhost:8080/"
 		googleAppKey = "18358026133409707365",
 		facebookAppKey = '165991020094008',
-		googleAnalyticsKey = 'UA-11129132-4';
+		googleAnalyticsKey = 'UA-11129132-4',
+		lastQuery = null,
+		timeout = null,
+		ajaxCall = null;
 	
 	this.query = function(terms, callback) {
-		console.log(terms);
+		if(timeout)
+			clearTimeout(timeout);
 		
-		$.getJSON(endpoint+"search.jsonp?q="+terms+"&callback=?", function(response) {
-			callback(response);
-	    });
+		timeout = setTimeout(function(){
+			if(ajaxCall)
+				ajaxCall.abort();
+			
+			if(terms == lastQuery)
+				return;
+			
+			lastQuery = terms;
+			ajaxCall = $.getJSON(endpoint+"search.jsonp?q="+terms+"&callback=?", function(response) {
+				callback(response);
+		    });
+		},150);
 	};
 };
